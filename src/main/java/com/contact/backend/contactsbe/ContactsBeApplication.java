75444f0db64b4ad495a0fda7418f2c9d010d97f4
zemikyafu <zemikfcb@gmail.com>
2023-03-04 14:27:00 +0200

    application property and main modified

--- src/main/java/com/contact/backend/contactsbe/ContactsBeApplication.java
+++ src/main/java/com/contact/backend/contactsbe/ContactsBeApplication.java
@@ -1,13 +1,22 @@
 package com.contact.backend.contactsbe;
 
+import com.contact.backend.contactsbe.security.JwtAuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 public class ContactsBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ContactsBeApplication.class, args);
-    }
 
+
+    }
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 }
