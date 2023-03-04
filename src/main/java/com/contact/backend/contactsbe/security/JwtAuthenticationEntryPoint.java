af5a1cd5f530b41fa6cfd0c6e0718dc6f07098bd
zemikyafu <zemikfcb@gmail.com>
2023-03-04 14:28:37 +0200

    jwt authentication added

---
+++ src/main/java/com/contact/backend/contactsbe/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.contact.backend.contactsbe.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+	@Override
+	public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+			throws IOException, ServletException {
+//		System.out.println("log getMethod"+ httpServletRequest.getMethod());
+//		System.out.println("log getRemoteUser "+ httpServletRequest.getRemoteUser());
+//		System.out.println("log getQueryString "+ httpServletRequest.getQueryString());
+		LOGGER.error("Responding with unauthorized error. Message - {}", e.getMessage());
+		httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Sorry, You're not authorized to access this resource.");
+	}
+
+}
