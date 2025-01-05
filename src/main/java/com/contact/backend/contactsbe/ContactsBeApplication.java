0c43934d2a023006665ea58d29ec6c361510060a
Zemichael Gebrekidan <58032220+zemikyafu@users.noreply.github.com>
2025-01-05 02:24:29 +0200

    Update ContactsBeApplication.java

--- src/main/java/com/contact/backend/contactsbe/ContactsBeApplication.java
+++ src/main/java/com/contact/backend/contactsbe/ContactsBeApplication.java
@@ -4,10 +4,7 @@ import com.contact.backend.contactsbe.security.JwtAuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
-//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 public class ContactsBeApplication {
     public static void main(String[] args) {
